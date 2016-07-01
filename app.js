094a8d2d9884d41741f4eb5936c0a99e38a26fac
Olu Wilson <oosajere@gmail.com>
2016-07-01 16:35:59 -0500

    new file

---
+++ app.js
@@ -0,0 +1,21 @@
+
+var fizz_Buzz = function() {
+  for (var i = 1; i <= 100; i++) {
+    var Result;
+     if ((i%3===0) && (i%5===0)){
+      Result = "FizzBuzz";
+    }
+   else if (i % 5 === 0) {
+      Result = "Buzz";
+    } 
+    else if (i % 3 === 0) {
+      Result = "Fizz";
+    }
+
+    else {
+      Result = i;
+    }
+      console.log(Result);
+  }
+};
+fizz_Buzz();
